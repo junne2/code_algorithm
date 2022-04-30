ad2854aa9ae0f84717ac5042503c0ea37538f888
ez_mon <87699427+junne2@users.noreply.github.com>
2022-04-30 19:26:45 +0900

    Create 1927_MinHip.py

---
+++ python/1927_MinHip.py
@@ -0,0 +1,16 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline())
+hip = []
+
+for i in range(n):
+    x = int(sys.stdin.readline())
+    
+    if not hip and x == 0:
+        print(0)
+    else:
+        if x > 0:
+            heapq.heappush(hip,x)
+        else:
+            print(heapq.heappop(hip))
