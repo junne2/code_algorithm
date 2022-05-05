d1dd82010be320cf671590d8ef5d3d2cb4d2c4ee
ez_mon <87699427+junne2@users.noreply.github.com>
2022-05-05 20:51:37 +0900

    Create 124nara.js

---
+++ javascript/124nara.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    var answer = '';
+    const number = [4,1,2];
+    
+    while (n){
+        answer = number[n % 3] + answer;
+        n = (n%3 === 0)? n / 3 - 1 : Math.floor(n / 3);
+    }
+    return answer;
+} 
